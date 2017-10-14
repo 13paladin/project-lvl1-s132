0b209ce615d0129b90b8ea951fab577316d96098
13paladin <geoaztec@gmail.com>
2017-10-14 10:52:45 +0300

    new logic

---
+++ src/bin/games/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { brainEven, brainGames } from '../..';
+
+brainGames(brainEven);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 import readlineSync from 'readline-sync';
+import { cons, car, cdr } from 'hexlet-pairs';
 
-export const brainGames = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!\n`);
+const brainEvenPlay = () => {
+  const numberMax = 100;
+  const number = Math.round(Math.random() * numberMax);
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  const userAnswer = readlineSync.question(`Question: ${number} `);
+  console.log(`Your answer: ${userAnswer}`);
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  return false;
 };
 
-export const brainEven = () => {
+const brainCalcPlay = () => {
+  const numberMax = 99;
+  const signVariants = '+-*';
+  const num1 = Math.round(Math.random() * numberMax);
+  const num2 = Math.round(Math.random() * numberMax);
+  const sign = signVariants[Math.round(Math.random() * (signVariants.length - 1))];
+  let correctAnswer;
+  switch (sign) {
+    case '+':
+      correctAnswer = num1 + num2;
+      break;
+    case '-':
+      correctAnswer = num1 - num2;
+      break;
+    case '*':
+      correctAnswer = num1 * num2;
+      break;
+    default:
+  }
+  const userAnswer = readlineSync.question(`Question: ${num1} ${sign} ${num2} `);
+  console.log(`Your answer: ${userAnswer}`);
+  if (Number(userAnswer) === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  return false;
+};
+
+const consGame = cons;
+export const brainEven = consGame('Answer "yes" if number even otherwise answer "no".\n', brainEvenPlay);
+export const brainCalc = consGame('What is the result of the expression?\n', brainCalcPlay);
+
+export const brainGames = (game) => {
   console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  console.log(car(game));
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
-  const numberMax = 10000;
-  const rounds = 3;
-  for (let i = 1; i <= rounds; i += 1) {
-    const number = Math.floor(Math.random() * numberMax);
-    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-    const userAnswer = readlineSync.question(`Question: ${number} `);
-    console.log(`Your answer: ${userAnswer}`);
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+  for (let i = 1; i <= 3; i += 1) {
+    if (!cdr(game)()) {
       console.log(`Let's try again, ${userName}!`);
       return;
     }
